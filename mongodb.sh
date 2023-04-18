4c778ce42b1f899456bb2f3131abc1389ec5b2a4
DELL <kolloju.aishwarya@gmail.com>
2023-04-19 01:48:33 +0530

    updating listen address from sed editor- spell check

--- mongodb.sh
+++ mongodb.sh
@@ -1,7 +1,7 @@
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 yum install mongodb-org -y
-sed -i -e 's|127.0.1|0.0.0.0|' /etc/mongod.conf
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
 systemctl enable mongod
-systemctl start mongod
+systemctl restart mongod
 
 #onfig file has to be edited : Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf
