5a320808fb4bbcfe93b536d1894a21cc2125e43e
DELL <kolloju.aishwarya@gmail.com>
2023-04-19 00:33:54 +0530

    copy locations adjusted

--- catalogue.sh
+++ catalogue.sh
@@ -1,13 +1,14 @@
-catalogue.sh
+
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 yum install nodejs -y
+cp catalogue.service /etc/systemd/system/catalogue.service
 useradd roboshop
 mkdir /app
 curl -o /tmp/catalogue.zip /https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 cd /app
 unzip /tmp/catalogue.zip
 npm install
-cp catalogue.service /etc/systemd/system/catalogue.service
+
 #Ensure you replace <MONGODB-SERVER-IPADDRESS> with IP address of mongodb in catalog service file
 systemctl daemon-reload
 systemctl enable catalogue
