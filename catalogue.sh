f1c3bc0c1f699ffb761f8206793623ea3af6229c
DELL <kolloju.aishwarya@gmail.com>
2023-04-19 02:15:59 +0530

    updating comments : echo "\e[36m >>> desrptive comment <<<<\e[0m"

--- catalogue.sh
+++ catalogue.sh
@@ -1,20 +1,30 @@
-
+echo "\e[36m>>>>>>>>>>>> configuring nodejs repos <<<<<<<<<<<<<\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 yum install nodejs -y
+echo "\e[36m >>>>>>> copy catalogue systemD file <<<<<<<<<<\e[om"
 cp catalogue.service /etc/systemd/system/catalogue.service
+echo "\e[36m >>>>>> copy the repo file for mongo client<<<<<<\e[0m"
 cp mongo.repo /etc/yum.repos.d/mongo.repo
+
+echo "\e[36m >>>>>>>create application directory and user <<<<<<<\e[0m"
 useradd roboshop
 mkdir /app
+
+echo "\e[36m >>>>>>download application content <<<<<<<\e[0m"
 curl -o /tmp/catalogue.zip /https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 cd /app
 unzip /tmp/catalogue.zip
+
+echo "\e[36m >>>> Install dependencies <<<<<<\e[0m"
 npm install
 
-#Ensure you replace <MONGODB-SERVER-IPADDRESS> with IP address of mongodb in catalog service file
+echo "\e[36m >>>>> start the services <<<<<<<\e[0m"
+
 systemctl daemon-reload
 systemctl enable catalogue
 systemctl restart catalogue
-
+echo "\e[36m >>>> Install mongodb client  <<<<<<<\e[0m"
 yum install mongodb-org-shell -y
+echo "\e[36m >>>> load schema <<<<<<<\e[0m"
 mongo --host mongodb-dev.e-platform.online </app/schema/catalogue.js
 
