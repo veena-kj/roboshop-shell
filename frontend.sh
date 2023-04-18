d29ddd8bc5a474387d6d515702443e1d11d13380
DELL <kolloju.aishwarya@gmail.com>
2023-04-18 20:23:05 +0530

    IP config's

--- frontend.sh
+++ frontend.sh
@@ -4,8 +4,5 @@ rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html/
 unzip /tmp/frontend.zip
-
-#some files to create and configure the ip
-
 systemctl enable nginx
-systemctl start nginx
+systemctl restart nginx
