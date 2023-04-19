d7996d82c36e2222a8cf77000ebab0d574e938a2
DELL <kolloju.aishwarya@gmail.com>
2023-04-20 02:16:15 +0530

    <test check>

--- frontend.sh
+++ frontend.sh
@@ -2,17 +2,15 @@ echo -e "\e[36m********* Install nginx **************\e[0m"
 yum install nginx -y
 
 echo -e "\e[36m********* Remove the content **************\e[0m"
-rm -rf /usr/share/nginx/html/*
-echo -e "\e[36m********* Download the content **************\e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
-echo -e "\e[36m********* navigate to content location & Extract app content**************\e[0m"
-cd /usr/share/nginx/html
+#rm -rf /usr/share/nginx/html/*
+#echo -e "\e[36m********* Download the content **************\e[0m"
+#curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+#echo -e "\e[36m********* navigate to content location & Extract app content**************\e[0m"
+#cd /usr/share/nginx/html
 pwd
-unzip /tmp/frontend.zip
+#unzip /tmp/frontend.zip
 systemctl enable nginx
 systemctl start nginx
 echo -e "\e[36m********* Configure reverse proxy **************\e[0m"
-cp /home/centos/roboshop-shell/roboshop.conf /etc/nginx/default.d/roboshop.conf
+#cp /home/centos/roboshop-shell/roboshop.conf /etc/nginx/default.d/roboshop.conf
 echo -e "\e[36m********* Enable nginx **************\e[0m"
-systemctl enable nginx
-systemctl restart nginx
