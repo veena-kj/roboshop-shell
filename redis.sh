b10f126fa9703ec0ca8bdb4c435192c9253ad7a2
DELL <kolloju.aishwarya@gmail.com>
2023-04-19 16:53:44 +0530

    schema check

--- redis.sh
+++ redis.sh
@@ -1,8 +1,12 @@
+echo -e "\e[36m*******Install Redis repos******\e[0m "
 yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+echo -e "\e[36m*******Install Redis*****\e[0m"
 dnf module enable redis:remi-6.2 -y
 yum install redis -y
+echo -e "\e[36m*******update listen address Redis*****\e[0m"
+sed -i -e s|127.0.0.1|0.0.0.0| /etc/redis.conf
 #Usually Redis opens the port only to localhost(127.0.0.1), meaning this service can be accessed by the application that is hosted on this server only. However, we need to access this service to be accessed by another server, So we need to change the config accordingly.
 #Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/redis.conf & /etc/redis/redis.conf
-
+echo -e "\e[36m*******Enable and start Redis service*****\e[0m"
 systemctl enable redis
 systemctl start redis
