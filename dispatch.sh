a6e54d969d3894fef279e92da57c75cac63ba33f
DELL <kolloju.aishwarya@gmail.com>
2023-04-19 21:40:37 +0530

    <messages >

--- dispatch.sh
+++ dispatch.sh
@@ -1,14 +1,32 @@
-cp dispatch.service /etc/systemd/system/dispath.service
+
+
+
+echo -e "\e[36m<<<<<<<<< Install golang >>>>>>>>\e[0m"
+pwd
 yum install golang -y
+echo -e "\e[36m<<<<<<<<< Create App user >>>>>>>>\e[0m"
 useradd roboshop
+echo -e "\e[36m<<<<<<<<< Create app directory >>>>>>>>\e[0m"
 mkdir /app
+pwd
+echo -e "\e[36m<<<<<<<<< Download the app content >>>>>>>>\e[0m"
 curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch.zip
+pwd
+echo -e "\e[36m<<<<<<<<< go to app directory >>>>>>>>\e[0m"
 cd /app
+echo -e "\e[36m<<<<<<<<< Unzip app content >>>>>>>>\e[0m"
 unzip /tmp/dispatch.zip
-cd /app
+pwd
+
+echo -e "\e[36m<<<<<<<<< Install dependencies >>>>>>>>\e[0m"
 go mod init dispatch
 go get
 go build
+echo -e "\e[36m<<<<<<<<< setup systemD service >>>>>>>>\e[0m"
+cp /home/centos/roboshop-shell/dispatch.service /etc/systemd/system/dispatch.service
+pwd
+echo -e "\e[36m<<<<<<<<< Reload Enable and start dispatch servie  >>>>>>>>\e[0m"
 systemctl daemon-reload
 systemctl enable dispatch
 systemctl start dispatch
+pwd
