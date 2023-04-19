ebdd00b98f9c6daf9e671b142ac285924fa9333a
DELL <kolloju.aishwarya@gmail.com>
2023-04-19 18:04:29 +0530

    messages added

--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,12 @@
+echo -e "\e[36m<<<<<<<<< Configuring mysql required version Repo files >>>>>>>>\e[0m"
 cp mysql.repo /etc/yum.repos.d/mysql.repo
+echo -e "\e[36m<<<<<<<<< disabling  default version repo files >>>>>>>>\e[0m"
 dnf module disable mysql -y
+echo -e "\e[36m<<<<<<<<< Install mysql server >>>>>>>>\e[0m"
 yum install mysql-community-server -y
+echo -e "\e[36m<<<<<<<<< Start&Enable mysqld >>>>>>>>\e[0m"
 systemctl enable mysqld
 systemctl start mysqld
+echo -e "\e[36m<<<<<<<<< Start using mysql servies with new root creds >>>>>>>>\e[0m"
 mysql_secure_installation --set-root-pass RoboShop@1
+
