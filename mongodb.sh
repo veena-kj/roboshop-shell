d59221fcf6f7934ff76129f4067302c3d71d6df5
DELL <kolloju.aishwarya@gmail.com>
2023-04-19 16:07:52 +0530

    message

--- mongodb.sh
+++ mongodb.sh
@@ -1,7 +1,12 @@
+echo -e "\e[36m ************ Creating mongodb repo file*******************\e[0m"
 cp mongo.repo /etc/yum.repos.d/mongo.repo
+echo -e "\e[36m ************ installing mongodb *******************\e[0m"
 yum install mongodb-org -y
+echo -e "\e[36m ************ update listen address in mongodb config file*******************\e[0m"
 sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
+echo -e "\e[26m ***********Enable and start services **********\e[0m"
+
 systemctl enable mongod
 systemctl restart mongod
 
-#onfig file has to be edited : Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf
+netstat -lntp
