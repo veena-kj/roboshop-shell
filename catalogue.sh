68f9eea7d3d9c3a16622cb52a514f8200ddd1018
DELL <kolloju.aishwarya@gmail.com>
2023-04-21 08:18:32 +0530

    < realpath in script_path variable access in files>

--- catalogue.sh
+++ catalogue.sh
@@ -2,8 +2,9 @@ script=(realpath $0)
 script_path = $(dirname $script)
 source ${script_path}/common.sh
 pwd
-echo script
-echo script_path
+echo ${script}
+echo ${script_path}
+pwd
 exit
 
 echo -e "\e[36m>>>>>>>>>>>> configuring nodejs repos <<<<<<<<<<<<<\e[0m"
