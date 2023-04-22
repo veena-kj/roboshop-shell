3a8e2d9ec6dff5a95fbea62857fde2b8f79387fb
DELL <kolloju.aishwarya@gmail.com>
2023-04-22 19:40:49 +0530

    <  password variable functionality >

--- common.sh
+++ common.sh
@@ -26,77 +26,97 @@ func_schema_setup(){
 func_heading "copy the repo file for mongo client"
 cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo &>>$logfile
 func_status_check $?
+
 func_heading "Install mongodb client"
 yum install mongodb-org-shell -y &>>$logfile
 func_status_check $?
+
 func_heading "load schema"
 mongo --host mongodb-dev.e-platform.online </app/schema/${component}.js &>>$logfile
 func_status_check $?
 fi
 
  if [ "$schema_setup" == "mysql" ]; then
+
  func_heading "copy mysql repo file"
 cp ${script_path}/mysql.repo /etc/yum.repos.d/mysql.repo &>>$logfile
 func_status_check $?
+
 func_heading "Install mysql client"
 yum install mysql -y &>>$logfile
 func_status_check $?
+
 func_heading "provide mysql root user passwd to interact with mysql to load schema"
 mysql -h mysql.e-platform.online -uroot -p${mysql_root_password} < /app/schema/${component}.sql &>>$logfile
 func_status_check $?
+
 fi
 
 }
 func_app_prereq(){
 
 func_heading "Create application user"
+
 id ${app_user} &>>$logfile
 if [ $? -ne 0 ]; then
 useradd ${app_user} &>>$logfile
 fi
 func_status_check $?
+
 func_heading "Create application directory "
 rm -rf /app &>>$logfile
 mkdir /app &>>$logfile
 func_status_check $?
+
 func_heading "Download the application content"
 curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>$logfile
 func_status_check $?
+
 func_heading "change to application directory"
 cd /app &>>$logfile
 func_status_check $?
+
 func_heading "Download the application content"
 unzip /tmp/${component}.zip &>>$logfile
 func_status_check $?
+
 }
 
 func_systemd_setup(){
+
   func_heading "create systemD service file"
   cp ${script_path}/${component}.service /etc/systemd/system/${component}.service &>>$logfile
   func_status_check $?
+
   func_schema_setup
-  func_status_check $?
+    func_status_check $?
+
   func_heading "Enable & Start ${component} Service"
   systemctl daemon-reload &>>$logfile
   systemctl enable ${component} &>>$logfile
   systemctl restart ${component} &>>$logfile
   func_status_check $?
+
 }
 
 
 
 func_nodejs(){
+
   func_heading  "Configuring NodeJs Repo files"
   curl -sL https://rpm.nodesource.com/setup_lts.x | bash  &>>$logfile
   func_status_check $?
+
   func_heading "Installing NodeJs Repo files"
   yum install nodejs -y &>>$logfile
  func_status_check $?
+
   func_app_prereq
 
   func_heading "Install NodeJs dependencies"
   npm install > /tmp/roboshop.log &>>$logfile
  func_status_check $?
+
   func_schema_setup
   func_systemd_setup
 }
@@ -106,15 +126,20 @@ func_java(){
 func_heading "Install Maven"
 yum install maven -y &>>$logfile
 func_status_check $?
+
 rm -rf /app &>>$logfile
+
 func_app_prereq
+
 func_heading "Install Dependencies for Maven"
 mvn clean package &>>$logfile
 func_status_check $?
+
 func_heading "move the file  generated"
 mv target/${component}-1.0.jar ${component}.jar &>>$logfile
 func_status_check $?
+
 func_schema_setup
 func_systemd_setup
-pwd
+
 }
--- mysql.sh
+++ mysql.sh
@@ -11,15 +11,19 @@ fi
 func_heading "disabling default version"
 dnf module disable mysql -y &>>$logfile
 func_status_check $?
+
 func_heading "Configuring mysql required version Repo files"
 cp ${script_path}/mysql.repo /etc/yum.repos.d/mysql.repo &>>$logfile
 func_status_check $?
+
 func_heading "Install mysql server"
 yum install mysql-community-server -y &>>$logfile
 func_status_check $?
+
 func_heading "Start using mysql services with new root creds"
 mysql_secure_installation --set-root-pass $mysql_root_password  &>>$logfile
 func_status_check $?
+
 func_heading "Start & Enable mysqld"
 systemctl enable mysqld &>>$logfile
 systemctl restart mysqld &>>$logfile
