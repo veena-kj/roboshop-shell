de0a9688bb36f71032ad9d98a0998e7c9de468a0
DELL <kolloju.aishwarya@gmail.com>
2023-04-22 08:09:29 +0530

    < varibale accessing >

--- mysql.sh
+++ mysql.sh
@@ -19,5 +19,5 @@ func_heading Start using mysql servies with new root creds
 mysql_secure_installation --set-root-pass ${mysql_root_password}
 func_heading "Start&Enable mysqld"
 systemctl enable mysqld
-systemctl start mysqld
+systemctl restart mysqld
 
