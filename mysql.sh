c99d4e82d969228122046a5b2df0ac64ecdd4f87
DELL <kolloju.aishwarya@gmail.com>
2023-05-24 20:37:38 +0530

    < password accessing >

--- mysql.sh
+++ mysql.sh
@@ -20,7 +20,7 @@ func_heading "Install mysql server"
 yum install mysql-community-server -y &>>$log_file
 func_status_check $?
 
-func_heading "Start using mysql services with new root creds- Rest MySQL Password"
+func_heading "Start using mysql services with new root creds- Reset MySQL Password"
 mysql_secure_installation --set-root-pass $mysql_root_password  &>>$log_file
 func_status_check $?
 
