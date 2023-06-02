cb0e094a8d3ac46b4e3ae9847ed512ae2bdd691d
root <root@ip-172-31-4-100.ec2.internal>
2023-06-02 13:40:15 +0000

    ss

--- mysql.sh
+++ mysql.sh
@@ -21,7 +21,7 @@ yum install mysql-community-server -y &>>$log_file
 func_status_check $?
 
 func_heading "Start using mysql services with new root creds- Reset MySQL Password"
-mysql_secure_installation --set-root-pass $mysql_root_password  &>>$log_file
+/usr/sbin/mysql_secure_installation --set-root-pass $mysql_root_password  &>>$log_file
 func_status_check $?
 
 func_heading "Start & Enable mysqld"
